d353f324b406651c9c66df1fe8b1c10fa839903f
hhhaiai <sanbo.xyz@gmail.com>
2022-01-28 13:18:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+module.exports = () => {
+  const data = { users: [] }
+  // Create 1000 users
+  for (let i = 0; i < 1000; i++) {
+    data.users.push({ id: i, name: `user${i}` })
+  }
+  return data
+}
